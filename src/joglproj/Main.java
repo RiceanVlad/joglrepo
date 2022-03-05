ebfa3475a41c61cf314021a83a10bf738533207a
vlad <ioan.ricean95@e-uvt.ro>
2022-03-05 14:23:45 +0200

    -template 1

--- src/joglproj/Main.java
+++ src/joglproj/Main.java
@@ -10,28 +10,30 @@ public class Main {
 
 	public static void main(String[] arguments){
 		
-		 //getting the capabilities object of GL2 profile        
-	      final GLProfile profile = GLProfile.get(GLProfile.GL2);
-	      GLCapabilities capabilities = new GLCapabilities(profile);
-	   
-	      // The canvas
-	      final GLCanvas glcanvas = new GLCanvas(capabilities);
-	      
-//	      Square instance = new Square();
-//	      Circle instance = new Circle();
-//	      Triangle instance = new Triangle();
-	      House instance = new House();
-	      
-	      glcanvas.addGLEventListener(instance);
-	      glcanvas.setSize(400, 400);
-	   
-	      //creating frame
-	      final JFrame frame = new JFrame ("straight Line");
-	   
-	      //adding canvas to frame
-	      frame.getContentPane().add(glcanvas);
-	                 
-	      frame.setSize(frame.getContentPane().getPreferredSize());
-	      frame.setVisible(true);    
+		new MainFrame();
+		
+//		 //getting the capabilities object of GL2 profile        
+//	      final GLProfile profile = GLProfile.get(GLProfile.GL2);
+//	      GLCapabilities capabilities = new GLCapabilities(profile);
+//	   
+//	      // The canvas
+//	      final GLCanvas glcanvas = new GLCanvas(capabilities);
+//	      
+////	      Square instance = new Square();
+////	      Circle instance = new Circle();
+////	      Triangle instance = new Triangle();
+//	      House instance = new House();
+//	      
+//	      glcanvas.addGLEventListener(instance);
+//	      glcanvas.setSize(400, 400);
+//	   
+//	      //creating frame
+//	      final JFrame frame = new JFrame ("straight Line");
+//	   
+//	      //adding canvas to frame
+//	      frame.getContentPane().add(glcanvas);
+//	                 
+//	      frame.setSize(frame.getContentPane().getPreferredSize());
+//	      frame.setVisible(true);    
 	}
 }
